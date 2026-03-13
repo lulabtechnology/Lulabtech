4ba5284ef44be3aba85b8802bdacbda8c709f50c
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2026-03-13 09:35:49 -0600

    Create navigation.ts

---
+++ data/navigation.ts
@@ -0,0 +1,8 @@
+import { NAV_ANCHORS } from "@/lib/constants";
+
+export const navigationItems = [
+  { label: "Proyectos", href: `#${NAV_ANCHORS.projects}` },
+  { label: "Servicios", href: `#${NAV_ANCHORS.services}` },
+  { label: "Por qué LulabTech", href: `#${NAV_ANCHORS.why}` },
+  { label: "Proceso", href: `#${NAV_ANCHORS.process}` }
+];
