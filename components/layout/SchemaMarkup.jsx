c5dfe335fbf72c1a81e0a4a47ffe0f6fa08e233d
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2026-03-12 20:31:23 -0600

    Update SchemaMarkup.jsx

--- components/layout/SchemaMarkup.jsx
+++ components/layout/SchemaMarkup.jsx
@@ -1,24 +1,64 @@
-import { absoluteUrl, contactEmail, contactPhone, brandName } from "@/lib/site";
+import {
+  absoluteUrl,
+  brandName,
+  contactEmail,
+  contactPhone,
+  defaultDescription,
+} from "@/lib/site";
 
 export default function SchemaMarkup() {
-  const schema = {
-    "@context": "https://schema.org",
-    "@type": "ProfessionalService",
-    name: brandName,
-    url: absoluteUrl("/"),
-    email: contactEmail,
-    telephone: contactPhone,
-    image: absoluteUrl("/logo-lulab.png"),
-    areaServed: ["Panamá", "Latinoamérica"],
-    serviceType: [
-      "Landing Pages",
-      "Webs Corporativas",
-      "Tiendas Online",
-      "Automatizaciones",
-    ],
-  };
+  const schemas = [
+    {
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      name: brandName,
+      url: absoluteUrl("/"),
+      logo: absoluteUrl("/logo-lulab.png"),
+      image: absoluteUrl("/logo-lulab.png"),
+      email: contactEmail,
+      telephone: contactPhone,
+      sameAs: [],
+    },
+    {
+      "@context": "https://schema.org",
+      "@type": "WebSite",
+      name: brandName,
+      url: absoluteUrl("/"),
+      description: defaultDescription,
+      inLanguage: "es",
+    },
+    {
+      "@context": "https://schema.org",
+      "@type": "ProfessionalService",
+      name: brandName,
+      url: absoluteUrl("/"),
+      description: defaultDescription,
+      email: contactEmail,
+      telephone: contactPhone,
+      image: absoluteUrl("/logo-lulab.png"),
+      areaServed: ["Panamá", "Latinoamérica"],
+      serviceType: [
+        "Landing Pages",
+        "Webs Corporativas",
+        "Tiendas Online",
+        "Automatizaciones",
+      ],
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "PA",
+      },
+    },
+  ];
 
   return (
-    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }} />
+    <>
+      {schemas.map((schema, index) => (
+        <script
+          key={index}
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      ))}
+    </>
   );
 }
