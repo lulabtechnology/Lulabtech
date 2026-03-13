74efae3107e253317ac9e46951e5c9d673745dd2
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2026-03-12 22:22:36 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { HomeCapabilitiesPreview } from "../components/sections/home-capabilities-preview";
 import { HomeCtaBand } from "../components/sections/home-cta-band";
+import { HomeDifferentiators } from "../components/sections/home-differentiators";
 import { HomeHero } from "../components/sections/home-hero";
 import { HomeIntro } from "../components/sections/home-intro";
+import { HomeProcess } from "../components/sections/home-process";
 import { HomeProofStrip } from "../components/sections/home-proof-strip";
 import { HomeShowcasePreview } from "../components/sections/home-showcase-preview";
 
@@ -13,6 +15,8 @@ export default function HomePage() {
       <HomeIntro />
       <HomeCapabilitiesPreview />
       <HomeShowcasePreview />
+      <HomeDifferentiators />
+      <HomeProcess />
       <HomeCtaBand />
     </>
   );
