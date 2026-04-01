01406bd4d45a9c30b29c0bf5f037c65c6ff19aa7
LuisDiaz1234 <131550504+LuisDiaz1234@users.noreply.github.com>
2026-04-01 14:09:43 -0600

    cd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import { Inter, Manrope } from "next/font/google";
 import { siteMetadata } from "@/lib/metadata";
 import { cn } from "@/lib/utils";
@@ -22,6 +23,8 @@ export const viewport: Viewport = {
   themeColor: "#0E76FF"
 };
 
+const metaPixelId = "1475809483955480";
+
 export default function RootLayout({
   children
 }: Readonly<{
@@ -36,6 +39,31 @@ export default function RootLayout({
           "min-h-screen bg-white font-sans text-ink-900"
         )}
       >
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${metaPixelId}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            alt=""
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
